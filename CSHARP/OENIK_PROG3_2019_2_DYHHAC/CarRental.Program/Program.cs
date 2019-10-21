07bd226c87f15e6509bf9ae9cca79e94f3cf1e6b
bozsobence <bozso.bence@stud.uni-obuda.hu>
2019-10-21 20:59:36 +0200

    project added

--- CSHARP/OENIK_PROG3_2019_2_DYHHAC/CarRental.Program/Program.cs
+++ CSHARP/OENIK_PROG3_2019_2_DYHHAC/CarRental.Program/Program.cs
@@ -1,14 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 namespace CarRental.Program
 {
-    class Program
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// It is the main class of the application.
+    /// </summary>
+    public class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// This is the main method of the program.
+        /// </summary>
+        /// <param name="args">
+        /// The arguments of the Main method.
+        /// </param>
+        public static void Main(string[] args)
         {
         }
     }
