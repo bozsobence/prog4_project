3b4c44452e4ecefe8b23c0d4c3005e965d93fff9
bozsobence <bozso.bence@stud.uni-obuda.hu>
2019-10-21 21:28:46 +0200

    CarRental.Logic added

---
+++ CSHARP/OENIK_PROG3_2019_2_DYHHAC/CarRental.Logic/LogicClasses.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="LogicClasses.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace CarRental.Logic
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// This it the container class of the logic layer.
+    /// </summary>
+    public class LogicClasses
+    {
+    }
+}
