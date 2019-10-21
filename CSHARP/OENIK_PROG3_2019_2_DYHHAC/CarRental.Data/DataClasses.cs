a79e6b8bdaa2e989d4ffbd06fc989d4ea4108690
bozsobence <bozso.bence@stud.uni-obuda.hu>
2019-10-21 21:12:04 +0200

    CarRental.Data project added

---
+++ CSHARP/OENIK_PROG3_2019_2_DYHHAC/CarRental.Data/DataClasses.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="DataClasses.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace CarRental.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// This is the container class of the data layer classes.
+    /// </summary>
+    public class DataClasses
+    {
+    }
+}
