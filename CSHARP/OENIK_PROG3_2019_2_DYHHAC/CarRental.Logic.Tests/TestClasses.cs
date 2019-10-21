61db29eefb552ffa190c8a68f393dc3e15b557d6
bozsobence <bozso.bence@stud.uni-obuda.hu>
2019-10-21 21:35:16 +0200

    CarRental.Logic.Tests added

---
+++ CSHARP/OENIK_PROG3_2019_2_DYHHAC/CarRental.Logic.Tests/TestClasses.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="TestClasses.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace CarRental.Logic.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// This is the container class of the tests.
+    /// </summary>
+    public class TestClasses
+    {
+    }
+}
