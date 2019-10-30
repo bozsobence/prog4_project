36282d13079b8c6de1c58a98dfd65d18786f8a55
bozsobence <bozso.bence@stud.uni-obuda.hu>
2019-10-30 16:25:36 +0100

    created container class for result classes of the logic functions

---
+++ CSHARP/OENIK_PROG3_2019_2_DYHHAC/CarRental.Logic/ResultClasses.cs
@@ -0,0 +1,68 @@
+﻿// <copyright file="ResultClasses.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace CarRental.Logic
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// This class contains the result classes of the <see cref="ILogic"/> functions.
+    /// </summary>
+    public static class ResultClasses
+    {
+        /// <summary>
+        /// This class contains the result of <see cref="ILogic.GetDailyIncome"/>.
+        /// </summary>
+        public class DailyIncomeResult
+        {
+            /// <summary>
+            /// Gets or sets the month.
+            /// </summary>
+            public int Month { get; set; }
+
+            /// <summary>
+            /// Gets or sets the day.
+            /// </summary>
+            public int Day { get; set; }
+
+            /// <summary>
+            /// Gets or sets the income.
+            /// </summary>
+            public int Income { get; set; }
+
+            /// <inheritdoc/>
+            public override string ToString()
+            {
+                return string.Format($"> DAY: {this.Day}\tINCOME: {this.Income} Ft");
+            }
+
+        }
+
+        /// <summary>
+        /// This class contains the result of <see cref="ILogic.get"/>.
+        /// </summary>
+        public class OverallIncomeResult
+        {
+            /// <summary>
+            /// Gets or sets the overall income.
+            /// </summary>
+            public int OverallIncome { get; set; }
+
+            /// <summary>
+            /// Gets or sets the average income per day.
+            /// </summary>
+            public double Average { get; set; }
+
+            /// <inheritdoc/>
+            public override string ToString()
+            {
+                return string.Format($"> OVERALL INCOME: {this.OverallIncome}\tAVERAGE DAILY INCOME: {this.Average}");
+            }
+        }
+    }
+}
