c0c79303667e4c2b6b532737781858a35d1ea28e
bozsobence <bozso.bence@stud.uni-obuda.hu>
2020-04-21 18:58:58 +0200

    view model created

---
+++ CSHARP/OENIK_PROG3_2019_2_DYHHAC/CarRental.Web/Models/AccountsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarRental.Web.Models
+{
+    public class AccountsViewModel
+    {
+        public Account CurrentlyEdited { get; set; }
+        public IEnumerable<Account> AccountsInDatabase { get; set; }
+    }
+}
